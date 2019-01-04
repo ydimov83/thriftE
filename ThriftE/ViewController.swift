af167e6dc9ddeb0626df91ae71a9a35724a9f79a
Yavor Dimov <ydimov@gmail.com>
2019-01-04 15:49:03 -0700

    Initial Commit

---
+++ ThriftE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThriftE
+//
+//  Created by Yavor Dimov on 1/4/19.
+//  Copyright © 2019 Yavor Dimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
