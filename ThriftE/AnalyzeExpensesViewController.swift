700b81cd5f85dcb41e3cd8e78393a16125cfd55e
Yavor Dimov <ydimov@gmail.com>
2019-02-18 18:34:57 -0700

    Tab Bar Controller refactor WIP

---
+++ ThriftE/AnalyzeExpensesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AnalyzeExpensesViewController.swift
+//  ThriftE
+//
+//  Created by Yavor Dimov on 2/18/19.
+//  Copyright © 2019 Yavor Dimov. All rights reserved.
+//
+
+import UIKit
+
+class AnalyzeExpensesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
