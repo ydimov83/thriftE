f5d13097c54e3550b5936f692567a7ddd2d7a350
Yavor Dimov <ydimov@gmail.com>
2019-03-19 17:55:28 -0600

    preliminary code for adding a collection view scroller to use on Analyze screen

--- ThriftE/AnalyzeExpensesViewController.swift
+++ ThriftE/AnalyzeExpensesViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 import Charts
 import CoreData
 
-class AnalyzeExpensesViewController: UIViewController, ChartViewDelegate {
+class AnalyzeExpensesViewController: UIViewController, ChartViewDelegate{
+    
     
     var managedObjectContext: NSManagedObjectContext! {
         didSet {
@@ -38,9 +39,13 @@ class AnalyzeExpensesViewController: UIViewController, ChartViewDelegate {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var pieChartView: PieChartView!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    
+//    @IBOutlet weak var collectionView: UICollectionView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+//        collectionView.dataSource = self  uncomment when implementing colleciton view
+//        collectionView.delegate = self uncomment when implementing collection view
+        
         if selectedDateSegment == "" {
             //If selectedDateSegment has no value then it means it's the first time user sees Analyze screen so segmentedControlIndexChanged() has not run yet, set category to Today and predicate accordingly
             selectedDateSegment = "Today"
@@ -57,6 +62,23 @@ class AnalyzeExpensesViewController: UIViewController, ChartViewDelegate {
         //Deselect the previously selected pie chart slice when coming back to the screen
         pieChartView.highlightValue(nil)
     }
+    
+    
+   
+        //TODO: Implement collectionView delegate methods, add , UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout  to class declaration. Also unhide collectionview from storyboard
+//    //MARK: - Collection View
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 3
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DateCollectionViewCell", for: indexPath) as! DateCollectionViewCell
+//        cell.dateLabelText = dateStrings[indexPath.row]
+//        cell.dateLabel.textColor = UIColor.black
+//
+//        return cell
+//    }
+    
     
     //MARK: - Actions
     @IBAction func segmentedControlIndexChanged(_ sender: Any) {
@@ -102,6 +124,16 @@ class AnalyzeExpensesViewController: UIViewController, ChartViewDelegate {
         fetchRequestPredicate = NSPredicate(format: "date >= %@ && date < %@", fromDate as CVarArg, toDate as CVarArg)
         setupChartData()
     }
+    
+    //TODO: Implement collectionView buttons, also unhide them from storyboard
+//    @IBAction func leftButton(_ sender: UIButton) {
+//        //        collectionView.beginInteractiveMovementForItem(at: <#T##IndexPath#>)
+//    }
+//
+//    @IBAction func rightButton(_ sender: UIButton) {
+//
+//    }
+
     
     //MARK: - Chart Setup
     func setupChartData() {
