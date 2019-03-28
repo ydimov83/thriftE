1443ee9688ce5324d71b97a17c12b490da52221d
Yavor Dimov <ydimov@gmail.com>
2019-03-28 10:16:41 -0600

    Unit test additions

--- ThriftETests/AnalyzeExpensesViewControllerTests.swift
+++ ThriftETests/AnalyzeExpensesViewControllerTests.swift
@@ -9,29 +9,9 @@
 import XCTest
 @testable import ThriftE
 
-class ThriftETests: XCTestCase {
-
-    var viewController: AnalyzeExpensesViewController?
-    override func setUp() {
-        super.setUp()
-        
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        let tabBarController = storyboard.instantiateInitialViewController() as! MyTabBarController
-        let tabBarViewControllers = tabBarController.viewControllers
-        let navigationController = tabBarViewControllers![1] as! UINavigationController
-     
-        viewController = navigationController.viewControllers[0] as! AnalyzeExpensesViewController
-        
-        let _ = navigationController.view
-        let _ = viewController!.view
-    }
-
-    override func tearDown() {
-        super.tearDown()
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+class AnalyzeExpensesViewControllerTests: BaseUnitTest {
 
     func testNoExpenseLabelIsHiddenOnLoad() {
-        XCTAssert(!viewController!.noExpensesLabel.isHidden, "no expenses label should be shown when screen is rendered with no data")
+        XCTAssert(!analyzeExpensesViewController!.noExpensesLabel.isHidden, "no expenses label should be shown when screen is rendered with no data")
     }
 }
---
+++ ThriftETests/BaseUnitTest.swift
@@ -0,0 +1,41 @@
+//
+//  BaseUnitTest.swift
+//  ThriftETests
+//
+//  Created by Yavor Dimov on 3/28/19.
+//  Copyright © 2019 Yavor Dimov. All rights reserved.
+//
+
+import XCTest
+import CoreData
+@testable import ThriftE
+
+class BaseUnitTest: XCTestCase {
+    var analyzeExpensesViewController: AnalyzeExpensesViewController?
+    var expenseListViewController: ExpenseListViewController?
+    let coreDataManager = CoreDataManager.sharedManager
+    
+    override func setUp() {
+        super.setUp()
+         coreDataManager.flushData()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let tabBarController = storyboard.instantiateInitialViewController() as! MyTabBarController
+        let tabBarViewControllers = tabBarController.viewControllers
+        let navigationController0 = tabBarViewControllers![0] as! UINavigationController
+        let navigationController1 = tabBarViewControllers![1] as! UINavigationController
+        
+        expenseListViewController = navigationController0.viewControllers[0] as? ExpenseListViewController
+        analyzeExpensesViewController = navigationController1.viewControllers[0] as? AnalyzeExpensesViewController
+        
+        let _ = navigationController0.view
+        let _ = expenseListViewController!.view
+        let _ = navigationController1.view
+        let _ = analyzeExpensesViewController!.view
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
