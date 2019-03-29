f2e206ba1d48d09f0813a9c26ef39db58cfde187
Yavor Dimov <ydimov@gmail.com>
2019-03-29 11:18:41 -0600

    a bit of refactoring

--- ThriftETests/CoreDataManagerTests.swift
+++ ThriftETests/CoreDataManagerTests.swift
@@ -11,51 +11,33 @@ import XCTest
 import CoreData
 @testable import ThriftE
 
-class CoreDataManagerTests: XCTestCase {
+class CoreDataManagerTests: BaseUnitTest {
     
-    var coreDataManager: CoreDataManager!
-    override func setUp() {
-        super.setUp()
-        
-        coreDataManager = CoreDataManager.sharedManager
-//
-//        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-//        let tabBarController = storyboard.instantiateInitialViewController() as! MyTabBarController
-//        let tabBarViewControllers = tabBarController.viewControllers
-//        let navigationController = tabBarViewControllers![1] as! UINavigationController
-//
-//        viewController = navigationController.viewControllers[0] as! AnalyzeExpensesViewController
-//
-//        let _ = navigationController.view
-//        let _ = viewController!.view
-    }
     
-    override func tearDown() {
-        super.tearDown()
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
-    func testCoreDataManagerInit() {
-        let coreDataInstance = CoreDataManager.sharedManager
-        XCTAssertNotNil(coreDataInstance, "CoreDataManager.sharedManager was not initialized")
+    func testCoreDataStackInitialization() {
+        XCTAssertNotNil(CoreDataManager.sharedManager, "CoreDataManager.sharedManager was not initialized")
+        XCTAssertNotNil(CoreDataManager.sharedManager.persistentContainer, "CoreDataManager's persistentContainer was not initialized")
+        XCTAssertNotNil(CoreDataManager.sharedManager.managedObjectContext, "CoreDataManager's managedObjectContext was not initialized")
     }
     
     func testInsertDeleteAndFlushExpense() {
         //Test insert success
         coreDataManager.insertExpense(name: "test1", amount: 30.00, category: "Misc", date: Date())
         coreDataManager.insertExpense(name: "test2", amount: 30.00, category: "Misc", date: Date())
-        XCTAssertTrue((coreDataManager?.isObjectInDataStore(name: "test1"))!, "Inserted object was not found in the data store")
-        XCTAssertTrue((coreDataManager?.isObjectInDataStore(name: "test2"))!, "Inserted object was not found in the data store")
+        coreDataManager.insertExpense(name: "test3", amount: 30.00, category: "Misc", date: Date())
+        XCTAssertTrue((coreDataManager.isObjectInDataStore(name: "test1")), "Inserted object was not found in the data store")
+        XCTAssertTrue((coreDataManager.isObjectInDataStore(name: "test2")), "Inserted object was not found in the data store")
+        XCTAssertTrue((coreDataManager.isObjectInDataStore(name: "test3")), "Inserted object was not found in the data store")
         //Test delete success
         coreDataManager.delete(name: "test1")
-        XCTAssertFalse((coreDataManager?.isObjectInDataStore(name: "test1"))!, "Deleted object should not be found in the data store")
-        XCTAssertTrue((coreDataManager?.isObjectInDataStore(name: "test2"))!, "Object which did not receive a 'delete' call should not have been deleted")
+        XCTAssertFalse((coreDataManager.isObjectInDataStore(name: "test1")), "Deleted object should not be found in the data store")
+        XCTAssertTrue((coreDataManager.isObjectInDataStore(name: "test2")), "Object which did not receive a 'delete' call should not have been deleted")
+        XCTAssertTrue((coreDataManager.isObjectInDataStore(name: "test3")), "Object which did not receive a 'delete' call should not have been deleted")
         //Test flush success
-        coreDataManager.insertExpense(name: "test3", amount: 30.00, category: "Misc", date: Date())
-        coreDataManager?.flushData()
-        XCTAssertFalse((coreDataManager?.isObjectInDataStore(name: "test1"))!, "Deleted object should not be found in the data store")
-        XCTAssertFalse((coreDataManager?.isObjectInDataStore(name: "test2"))!, "Deleted object should not be found in the data store")
-        XCTAssertFalse((coreDataManager?.isObjectInDataStore(name: "test3"))!, "Deleted object should not be found in the data store")
+        coreDataManager.flushData()
+        XCTAssertFalse((coreDataManager.isObjectInDataStore(name: "test1")), "Deleted object should not be found in the data store")
+        XCTAssertFalse((coreDataManager.isObjectInDataStore(name: "test2")), "Deleted object should not be found in the data store")
+        XCTAssertFalse((coreDataManager.isObjectInDataStore(name: "test3")), "Deleted object should not be found in the data store")
     }
     
     
